e6d0adb0c70ced59e2c8768bc5a85e6e75056973
Shashwat <shashwat12028@gmail.com>
2024-07-08 11:58:29 +0530

    Work on career page going on

---
+++ app/careers/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+import toast, { Toaster } from "react-hot-toast";
+import CareerNode from "@/components/CareerNode";
+import { uploaderOptions } from "@/lib/utils";
+import { UrlBuilder } from "@bytlescale/sdk";
